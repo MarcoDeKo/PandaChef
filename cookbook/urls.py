4e244cb540be8ab6305764e91440ef7355caf8ad
Marco de Koning <marcoadk@gmail.com>
2019-06-29 14:13:47 +0200

    Home and item view added

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from cookbook import views
 
 urlpatterns = [
-    path("", views.home, name="cookbook home")
+    path("", views.home, name="cookbook home"),
+    path("<name>", views.item, name="cookbook home")
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home!")
+    return render(
+        request,
+        'cookbook/home.html'
+    )
+
+def item(request, name):
+    return render(
+        request,
+        'cookbook/item.html',
+        {
+            'name': name
+        }
+    )
